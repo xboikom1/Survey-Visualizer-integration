c85b1fc5ab271a1a3592987d8fd144984f77ae84
xboikom1 <Maksimboiko222@gmail.com>
2025-10-03 23:43:31 +0200

    add header content and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
+import css from "./Header.module.css";
+
 const Header = () => {
   return (
-    <header>
-      <h1>Trivia Questions Visualization</h1>
+    <header className={css.header}>
+      <div className={css.titleContainer}>
+        <h1 className={css.title}>Trivia Visualizer</h1>
+        <h2 className={css.subtitle}>
+          Interactive data visualization tool for exploring trivia questions
+          from the Open Trivia Database
+        </h2>
+      </div>
     </header>
   );
 };
