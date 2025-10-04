6f0ab51222b7f630ce3d757be846074a143be20b
xboikom1 <Maksimboiko222@gmail.com>
2025-10-04 16:10:05 +0200

    update HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,73 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "../../components/Header/Header";
 import css from "./HomePage.module.css";
+import { fetchCategories } from "../../redux/categories/operations";
+import {
+  selectCategories,
+  selectCategoriesLoading,
+  selectCategoriesError,
+} from "../../redux/categories/selectors";
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+  const categories = useSelector(selectCategories);
+  const loading = useSelector(selectCategoriesLoading);
+  const error = useSelector(selectCategoriesError);
+
+  useEffect(() => {
+    if (categories?.length === 0 && !loading) {
+      dispatch(fetchCategories());
+    }
+  }, [dispatch, categories?.length, loading]);
+
   return (
-    <div className={css.pageContainer}>
+    <>
       <Header />
-    </div>
+      <main className={css.pageContainer}>
+        <section id="overview" className={css.overviewSection}>
+          <h2 className={css.overviewTitle}>Discover the World of Trivia</h2>
+          <ul className={css.overviewStats}>
+            <li className={css.statCard}>
+              <span className={css.statNumber}>50+</span>
+              <p className={css.statLabel}>Questions</p>
+            </li>
+            <li className={css.statCard}>
+              <span className={css.statNumber}>10+</span>
+              <p className={css.statLabel}>Categories</p>
+            </li>
+            <li className={css.statCard}>
+              <span className={css.statNumber}>3</span>
+              <p className={css.statLabel}>Difficulty Levels</p>
+            </li>
+          </ul>
+        </section>
+
+        <section id="categories" className={css.categoriesSection}>
+          <h2 className={css.categoriesTitle}>Categories</h2>
+          <p className={css.categoriesDescription}>
+            Explore the distribution of questions across different knowledge
+            categories
+          </p>
+          {loading ? (
+            <p>Loading categories...</p>
+          ) : error ? (
+            <p>{error}</p>
+          ) : (
+            categories?.length > 0 && (
+              <ul className={css.categoriesGrid}>
+                {categories.map((category) => (
+                  <li key={category.id} className={css.categoryCard}>
+                    <h3>{category.name}</h3>
+                    <p>{category.questionCount} questions in this category</p>
+                  </li>
+                ))}
+              </ul>
+            )
+          )}
+        </section>
+      </main>
+    </>
   );
 };
 
--- src/redux/categories/operations.js
+++ src/redux/categories/operations.js
@@ -4,11 +4,23 @@ import axios from "axios";
 axios.defaults.baseURL = "https://opentdb.com";
 
 export const fetchCategories = createAsyncThunk(
-  "categories/fetchCategories",
+  "categories/fetchCategoriesWithQuestionCounts",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/api_category.php");
-      return response.data.trivia_categories;
+      const categoriesResponse = await axios.get("/api_category.php");
+      const categories = categoriesResponse.data.trivia_categories;
+
+      const categoriesWithCounts = await Promise.all(
+        categories.map(async (category) => {
+          const questionsResponse = await axios.get(
+            `/api_count.php?category=${category.id}`
+          );
+          const questionCount =
+            questionsResponse.data.category_question_count.total_question_count;
+          return { ...category, questionCount };
+        })
+      );
+      return categoriesWithCounts;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
